04daa10d9b0223fce07f954181fd9fc2aae68f36
tomekaks <tomekaks@gmail.com>
2022-05-09 20:00:25 +0200

    Added AddMealsController and AddMealsVM

---
+++ MyCalorieCounter/Controllers/AddMealsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCalorieCounter.Application.Interfaces.Services;
+using MyCalorieCounter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCalorieCounter.Controllers
+{
+    public class AddMealsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public AddMealsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _productService.GetProductList();
+            var model = new AddMealsVM();
+            model.Products = productList;
+            //foreach (var item in productList)
+            //{
+            //    products.Add(new ProductVM
+            //    {
+            //        Name = item.Name,
+            //        Calories = item.Calories,
+            //        Proteins = item.Proteins,
+            //        Carbs = item.Carbs,
+            //        Fats = item.Fats
+            //    });
+            //}
+            return View(model);
+        }
+    }
+}
---
+++ MyCalorieCounter/Models/AddMealsVM.cs
@@ -0,0 +1,19 @@
+﻿using MyCalorieCounter.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCalorieCounter.Models
+{
+    public class AddMealsVM
+    {
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Proteins { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+        public List<ProductDto> Products { get; set; }
+        public int Weight { get; set; }
+    }
+}
